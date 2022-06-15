f843c171126df20a4cd2f0e8663502e99bc1d05a
Jônatas Felix <jon.cunha@hotmail.com>
2022-06-15 14:59:28 -0300

    Dividindo etapas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+import Logo from "./img/logo.png"
 
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+`
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+`
+const Imagem = styled.img`
+  width: 180px;
+`
+
+const Main = styled.main`
+  display: grid;
+  grid-template-columns: repeat(20, 1fr);
+`
+
+const Filtros = styled.div`
+  grid-column: 1/span 4;
+  background-color: green;
+`
+
+const Produtos = styled.div`
+  grid-column: 5/span 12;
+  background-color: yellow;
+
+`
+
+const Carrinho = styled.div`
+  grid-column: 17/span 4;
+  background-color: blue;
+
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <Header><Imagem src={Logo} alt="" /></Header>
+      <Main>
+        <Filtros></Filtros>
+        <Produtos></Produtos>
+        <Carrinho></Carrinho>
+      </Main>
+      <footer></footer>
+    </Container>
   );
 }
 
